1c9d5e1d2c3ccb2ec67e8195387008b7230b9c5f
Nirei Orange <nireiorange@gmail.com>
2017-04-18 12:30:49 +0200

    Diccionario de queries adaptado para trabajar con el parser de queries. Terminada la clase de informes.

--- mri-searcher/src/main/java/mri_searcher_util/DiccionarioQueries.java
+++ mri-searcher/src/main/java/mri_searcher_util/DiccionarioQueries.java
@@ -1,14 +1,27 @@
 package mri_searcher_util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import mri_searcher_parsers.Cran_FileReader;
+
 public class DiccionarioQueries {
+	
+	static ArrayList<String> lista = Cran_FileReader.readQuery("cran/cran.qry");
+	static ArrayList<String> relev = Cran_FileReader.readRelevance("cran/cranqrel");
 
 	public static final String getContent(int query) {
-		throw new UnsupportedOperationException("Not implemented");
+		return lista.get(query-1);
 	}
 	
 	public static final List<Integer> getRelevants(int query) {
-		throw new UnsupportedOperationException("Not implemented");
+		String[] rels = relev.get(query-1).split(",");
+		Integer[] irels = new Integer[rels.length];
+		for(int i=0; i<rels.length; i++) { // Parseamos todos los enteros
+			irels[i] = Integer.parseInt(rels[i]);
+		}
+		return Arrays.asList(irels);
+		
 	}
 }
--- mri-searcher/src/main/java/mri_searcher_util/Visualizar.java
+++ mri-searcher/src/main/java/mri_searcher_util/Visualizar.java
@@ -16,11 +16,12 @@ public class Visualizar {
 	private static final int P20 = 2;
 	private static final int R20 = 3;
 	
-	private static String presentarQuery(int query, IndexReader reader, TopDocs topDocs, final String[] fields, float[] metricas) throws IOException {
-		StringBuilder sb = new StringBuilder();
+	private static void presentarQuery(StringBuilder sb, int query, IndexReader reader, TopDocs topDocs, final String[] fields, float[] metricas) throws IOException {
 		String queryName = DiccionarioQueries.getContent(query);
 		List<Integer> resultados = new ArrayList<>();
 		List<Integer> relevantes = DiccionarioQueries.getRelevants(query);
+		
+		sb.append("Query " + query + ": " + queryName); // 1 PRINT: LA QUERY
 		
 		// Obtenemos los documentos
 		for(ScoreDoc scoreDoc : topDocs.scoreDocs) {
@@ -28,12 +29,18 @@ public class Visualizar {
 			String score = Float.toString(scoreDoc.score);
 			
 			// añadimos la ID propia del documento a la lista de resultados
-			resultados.add(Integer.parseInt(doc.getField("I").stringValue()));
+			int campoI = Integer.parseInt(doc.getField("I").stringValue());
+			resultados.add(campoI);
+			
+			sb.append(doc.getField("I").stringValue()); // 2 PRINT: EL DOCID (CAMPO I)
+			sb.append(relevantes.contains(campoI) ? " (RELEVANTE)" : "");  // 3 PRINT: MARCA DE RELEVANCIA 
 			
 			// Obtenemos los campos
 			for(String f : fields) {
-				sb.append(f + " : " + doc.getField(f).stringValue() );
+				sb.append(f + " : " + doc.getField(f).stringValue() + "\n"); // 4 PRINT: LOS CAMPOS REQUERIDOS
 			}
+			
+			sb.append("Score: " + score); // 5 PRINT: EL SCORE
 		}
 		
 		// Obtenemos las métricas
@@ -42,13 +49,27 @@ public class Visualizar {
 		metricas[P20] = Metricas.Patn(20, resultados, relevantes);
 		metricas[R20] = Metricas.Recallatn(20, resultados, relevantes);
 		
-		sb.append("Métricas: P@10 " + metricas[P10] + " Recall@10 " + metricas[R10] + " P@20 " + metricas[P20] + " Recall@20 " + metricas[R20]);
-		
-		return null;
-		
+		sb.append("Métricas: P@10 " + metricas[P10] + " Recall@10 " + metricas[R10] + " P@20 " + metricas[P20] + " Recall@20 " + metricas[R20]);		
 	}
 	
-	public static final String visualizar() {
-		return null;
+	public static final String visualizar(int[] queries, IndexReader reader, TopDocs topDocs, final String[] fields) throws IOException {
+		float sumP10 = 0f, sumR10 = 0f, sumP20 = 0f, sumR20 = 0f;
+		float[] metricas = new float[4];
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<queries.length; i++) {
+			presentarQuery(sb, queries[i], reader, topDocs, fields, metricas);
+			sumP10 += metricas[P10];
+			sumR10 += metricas[R10];
+			sumP20 += metricas[P20];
+			sumR20 += metricas[R20];
+		}
+		
+		sb.append("Promedio métricas: ");
+		sb.append("P@10 " + sumP10/queries.length);
+		sb.append("Recall@10 " + sumR10/queries.length);
+		sb.append("P@20 " + sumP20/queries.length);
+		sb.append("Recall@20 " + sumR20/queries.length);
+		
+		return sb.toString();
 	}
 }
